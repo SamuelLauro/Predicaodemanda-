2fd0be3aefff72aa58517f9b39e0e604cfa18ff1
Samuel Lauro <samuellauro0612@gmail.com>
2024-10-05 14:39:52 -0300

    .

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
+import pickle
 from flask import Flask, request, render_template
-import pickle  # Ou 'joblib' se estiver usando joblib
 import numpy as np
 
 app = Flask(__name__)
 
-# Carregar o modelo recriado
-model = pickle.load(open('model.pkl', 'rb'))
+# Carregar o modelo salvo
+with open('modelo.pkl', 'rb') as file:
+    modelo = pickle.load(file)
 
 @app.route('/')
 def home():
@@ -13,14 +14,14 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Coletar os dados enviados pelo formulário
+    # Obter os dados do formulário enviado pelo usuário
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     
     # Fazer a previsão
-    prediction = model.predict(final_features)
+    prediction = modelo.predict(final_features)
     
-    return render_template('index.html', prediction_text=f'Previsão: {prediction}')
+    return render_template('index.html', prediction_text=f'Previsão de Frequência: {prediction[0]} dias')
 
 if __name__ == "__main__":
     app.run(debug=True)
